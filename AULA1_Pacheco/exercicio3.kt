06a82137ebaa235c12a641fc6424728b7bf6c811
yasminlaia <30813654+yasminlaia@users.noreply.github.com>
2018-09-27 21:33:14 -0300

    Update exercicio3.kt

--- AULA1_Pacheco/exercicio3.kt
+++ AULA1_Pacheco/exercicio3.kt
@@ -24,4 +24,45 @@ fun main(args: Array<String>) {
     
     casaJoao.abrirJanela()
     casaJoao.abrirPorta()
+	
+/**parte 2**/
+
+/**relembrando utilização de classes**/
+/**casaJoao --> instancia criada**/
+
+class Casa(cor:String, qtGaragem:Int){
+	//Propriedades
+	var cor : String 
+    var garagem : Int
+    
+    //Construtor
+    init{
+        this.cor = cor
+        this.garagem = qtGaragem
+        println("quantidade de garagens é $garagem")
+    }
+    
+    //Métodos
+    fun abrirPorta(qt1:Int){
+        println("temos $qt1 portas abertas na casa do João")
+    }
+    fun abrirJanela(qt2:Int){
+        println("temos $qt2 janelas abertas na casa do João")
+    }
+    fun pintar(cor:String){
+        this.cor = cor
+        println("$cor é a cor da casa do João")
+    }
+    fun abrirCasa(){
+        this.abrirPorta(3)
+        this.abrirJanela(2)        
+    }
+    
+}
+
+fun main(args: Array<String>) {
+	var casaJoao = Casa("Verde",2)    
+    casaJoao.abrirCasa()
+    casaJoao.pintar("Verde")
+}
     
