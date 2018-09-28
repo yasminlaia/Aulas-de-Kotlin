e2d364a45142e80aeb311965d1220479065cd216
yasminlaia <30813654+yasminlaia@users.noreply.github.com>
2018-09-27 22:08:36 -0300

    exercicio5.tk
    entendendo as funcionalidades do kotlin que estão comentadas no código

---
+++ AULA1_Pacheco/exercicio5.kt
@@ -0,0 +1,31 @@
+//private serve para deixar disponivel apenas uma classe em especifico
+//private --> Visível apenas na classe
+//protected --> Visivel na classe e nas classes
+//public --> Visível sempre (é o padrão)
+//internal --> Visível dentro do módulo
+
+class ContaBancaria(saldoInicial:Int){
+    private var saldo: Double
+    
+    init {
+        this.saldo = saldoInicial
+    }
+    fun consulta(){
+    	println("Seu saldo é $saldo")
+    }
+    fun deposito(valor:Double){
+        this.saldo = this.saldo + valor
+        
+    }
+    fun saque(valor:Double){
+        this.saldo = this.saldo - valor
+    }
+}
+
+fun main(args:Array<String>){
+	val cc = ContaBancaria(100.00)
+    
+    cc.consulta()
+    cc.deposito(20.00)
+    cc.consulta()
+}
