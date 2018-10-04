140f4026d030ef20bf1260baee7f0fbc39f2f09e
yasminlaia <30813654+yasminlaia@users.noreply.github.com>
2018-10-04 20:01:42 -0300

    Update exercicio3.kt

--- AULA2_PACHECO/exercicio3.kt
+++ AULA2_PACHECO/exercicio3.kt
@@ -1,3 +1,4 @@
+#USANDO CONTADOR MAIS RESULTANDO EM UM NUMERO CRESCENTE A PARTIR DO #0
 fun main(args: Array<String>) {
     var contador = 0
     
@@ -5,5 +6,16 @@ fun main(args: Array<String>) {
     	println("Contador = $contador")
         //contador = contador + 1
         contador++
+    }
+}
+
+#USANDO CONTADOR MENOS RESULTANDO EM UM NUMERO DECRESCENTE A PARTIR DO #5
+fun main(args: Array<String>) {
+    var contador = 5
+    
+    while(contador>0){
+    	println("Contador = $contador")
+        //contador = contador - 1
+        contador--
     }
 }
