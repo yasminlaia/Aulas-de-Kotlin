398b27fdcc161c0322c70cf85ec2d8d51a67335b
yasminlaia <30813654+yasminlaia@users.noreply.github.com>
2018-10-04 19:58:16 -0300

    exercicio3.kt
    relembrando o conceito de contador em kotlin

---
+++ AULA2_PACHECO/exercicio3.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    var contador = 0
+    
+    while(contador<5){
+    	println("Contador = $contador")
+        //contador = contador + 1
+        contador++
+    }
+}
