c8e13e3a8e23ef5e7642c84bef7e3ad5306d71db
yasminlaia <30813654+yasminlaia@users.noreply.github.com>
2018-10-04 19:32:56 -0300

    exercicio1.kt

---
+++ AULA2_PACHECO/exercicio1.kt
@@ -0,0 +1,27 @@
+/**------------------------------------------------------------------------------------------------------------------------------------**/
+fun main(args: Array<String>) {
+	var idade = 14
+    
+    if (idade<16){
+        println("Não pode votar")
+    }
+    if (((idade >= 16) && (idade<18)) || (idade>60)){
+        println("Pode votar")
+    }
+    if ((idade>=18) && (idade<60)){
+        println("Deve votar")
+    }
+}
+/**--CÓDIGO PARA REPRESENTAR O CASE EM OUTRAS LINGUAGENS----------------------------------------------------------------------------------------------------------------------------------**/
+fun main(args: Array<String>) {
+	var opcao = 4
+    
+    when(opcao){
+        1 -> {
+            println("Café com leite")
+        	println("Com desconto")
+        }
+        2,3 -> println("Chocolate quente")
+        else -> println("Opção inválida")
+    }
+}
