2ca806f3d1e34b2f7cc7e9ad1e9f278aca0e342b
yasminlaia <30813654+yasminlaia@users.noreply.github.com>
2018-10-04 20:29:11 -0300

    exercicio5.kt
    utilizando implementações

---
+++ AULA2_PACHECO/exercicio5.kt
@@ -0,0 +1,25 @@
+   /*
+     * Collections ou coleções são implementações estrutura de dados
+     * -List>> Imutável, tamanho fixo, você colocar ou remover elementos.
+    */
+    
+
+fun main(args: Array<String>) {
+
+    var listaItens = arraylistOf("MS","SP","RJ","BA")
+    println(listaItens)
+	  println(listaItens.size)
+	  println(listaItens.isEmpty)
+    println(listaItens[3])
+    listaItens[0] = "RS"
+    
+    listaItens.add("SP")
+    println(listaItens)
+    
+	listaItens.remove("SP") //Remove o primeiro SP
+    println(listaItens)
+    
+    listaItens.removeAt(2) //remove o elemento da posição 2
+	println(listaItens)
+    
+	}
